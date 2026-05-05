09cbd5c7cfb4826b2a05a481169c4f98fc2227e2
Harlan Bullock <harlanbull7@gmail.com>
2026-05-05 16:56:46 -0700

    Adds navigation between pages via navbar component

--- shadesmar/src/components/navbar.tsx
+++ shadesmar/src/components/navbar.tsx
@@ -1,26 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="w-full bg-[#5eccf3] px-6 py-4 shadow-md">
       <div className="mx-auto flex max-w-6xl items-center justify-between">
-        <h1 className="text-xl font-bold text-white">
+        <h1 className="text-xl font-bold text-gray-900">
           Shadesmar
         </h1>
 
-        <ul className="flex gap-6 text-white font-medium">
+        <ul className="flex gap-6 text-gray-900 font-medium">
           <li>
-            <a href="#" className="hover:text-slate-100">
-              Home
-            </a>
+			<Link to="/" className="hover:text-slate-100">
+				Home
+			</Link>
           </li>
           <li>
-            <a href="#" className="hover:text-slate-100">
-              About
-            </a>
+            <Link to="/map" className="hover:text-slate-100">
+              Explore
+            </Link>
           </li>
-          <li>
-            <a href="#" className="hover:text-slate-100">
-              Contact
-            </a>
+          <li> 
+            <Link to="/profile" className="hover:text-slate-100">
+              Profile
+            </Link>
           </li>
         </ul>
       </div>
