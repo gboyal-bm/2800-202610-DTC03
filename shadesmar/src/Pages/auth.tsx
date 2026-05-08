f2c85e2ade2690dfde516673cc93dd704c85ef8c
Al0102 <alu59@my.bcit.ca>
2026-05-08 12:00:19 -0700

    Add submit

--- shadesmar/src/Pages/auth.tsx
+++ shadesmar/src/Pages/auth.tsx
@@ -1,4 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ShadesmarApi } from "../utils/shadesmar_api";
+import { useState } from "react";
+
 type AuthMode = "login" | "register";
 
 interface AuthModes {
@@ -6,6 +9,39 @@ interface AuthModes {
 }
 
 export function Auth({ mode }: AuthModes) {
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password: '',
+        rememberMe: true,
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type, checked } = event.target;
+        setForm(prevForm => ({
+            ...prevForm,
+            [name]: type === 'checkbox' ? checked : value,
+        }));
+    }
+
+    const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("Submitting form:", form);
+        try {
+            const result = await ShadesmarApi.apiFetch(`/auth/${mode}`, {
+                method: "POST",
+                body: JSON.stringify(form),
+            });
+            console.log("Authentication result:", result);
+            if (result.status === 200) {
+                navigate("/exploration");
+            }
+        } catch (err) {
+            console.error("Authentication error:", err);
+        }
+    };
+
     return (
         <main className="max-w-2xl mx-auto my-6 mb-28">
             <h1 className="text-5xl py-12 font-bold">
@@ -17,46 +53,69 @@ export function Auth({ mode }: AuthModes) {
                         <label className="flex text-lg flex-1 items-center font-semibold">
                             Email:
                         </label>
+                        {mode === "register" && (
+                            <>
+                                <label className="flex text-lg flex-1 items-center font-semibold">
+                                    Username
+                                </label>
+                            </>
+                        )}
                         <label className="flex text-lg flex-1 items-center font-semibold">
                             Password:
                         </label>
                     </div>
-                    <div className="flex flex-1 flex-col gap-8">
+                    <form className="flex flex-1 flex-col gap-8" onSubmit={handleSubmit}>
+                        {mode === "register" && (
+                            <>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    className="flex flex-1 border p-2 rounded-md w-full bg-white"
+                                    placeholder="Username"
+                                    onChange={handleChange}
+                                />
+                            </>
+                        )}
                         <input
                             type="email"
+                            name="email"
                             className="flex flex-1 border p-2 rounded-md w-full bg-white"
                             placeholder="Email"
+                            onChange={handleChange}
                         />
                         <input
                             type="password"
+                            name="password"
                             className="flex flex-1 border p-2 rounded-md w-full bg-white"
                             placeholder="Password"
+                            onChange={handleChange}
                         />
-                    </div>
+                        <input type="submit" value={mode === "login" ? "Log In" : "Register"} />
+                    </form>
                 </div>
                 <div className="flex flex-1 flex-col gap-4">
                     <button className="border py-4 rounded-lg text-lg font-bold bg-white">
-                        Log In
+                        {mode === "login" ? "Log In" : "Register"}
                     </button>
-                    <p>
-                        {mode == "login" ? (
-                            <>
-                                No account?{" "}
-                                <Link to="/register" className="underline text-blue-800">
-                                    Sign up
-                                </Link>
-                            </>
-                        ) : (
-                            <>
-                                Already have an account?{" "}
-                                <Link to="/login" className="underline text-blue-800">
-                                    Log in
-                                </Link>
-                            </>
-                        )}
-                    </p>
                 </div>
+                <p>
+                    {mode == "login" ? (
+                        <>
+                            No account?{" "}
+                            <Link to="/register" className="underline text-blue-800">
+                                Sign up
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            Already have an account?{" "}
+                            <Link to="/login" className="underline text-blue-800">
+                                Log in
+                            </Link>
+                        </>
+                    )}
+                </p>
             </div>
-        </main>
+        </main >
     );
 }
