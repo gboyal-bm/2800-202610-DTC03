2e2c1e273a2cd50e69698954b3fa481eae3a91c9
gus <gustavosrod1121@gmail.com>
2026-05-13 11:29:01 -0700

    Create a recommendActivity function with additional context

--- shadesmar/src/utils/gemini.ts
+++ shadesmar/src/utils/gemini.ts
@@ -10,27 +10,32 @@ export async function sendMessage(message: string) {
     });
 
     const data = await response.json();
-
     return data.response;
 }
 
 export async function recommendActivities(info: string) {
-    // const response = await ai.models.generateContent({
-    //     model: "gemini-3.1-flash-lite",
-    //     contents: `
-    //     You are a chatbot in our app, and your job is to recommend activities to the user. Here is a description:
-    //     '''
-    //     Our team is developing a web application called Shadesmar aimed towards residents of Vancouver that locates activities, community centres, and parks to encourage exploring the city while staying cool, using a gamified progression system to make it fun. 
-    //     '''
+    const response = await fetch("/api/ai/generateMessage", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            info: `
+                You are a chatbot in our app, and your job is to recommend activities to the user. Here is a description:
+                '''
+                Our team is developing a web application called Shadesmar aimed towards residents of Vancouver that locates activities, community centres, and parks to encourage exploring the city while staying cool, using a gamified progression system to make it fun. 
+                '''
 
-    //     Using the info below, recommend an activity for this user. Here is the info:
-    //     '''
-    //     ${info}
-    //     '''
+                Using the info below, recommend an activity for this user. Here is the info:
+                '''
+                ${info}
+                '''
 
-    //     Don't break character, and don't give a long answer. Please keep it concise, one or two sentences maximum.
-    //     `,
-    // });
-    console.log("Info:", info);
-    return info;
+                Don't break character, and don't give a long answer. Please keep it concise, one or two sentences maximum.
+            `,
+        }),
+    });
+
+    const data = await response.json();
+    return data.response;
 }
