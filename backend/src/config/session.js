64fe45e85cca28962b84e9e903b8fb7371baea84
Al0102 <alu59@my.bcit.ca>
2026-05-14 11:26:22 -0700

    Add MongoStore

--- backend/src/config/session.js
+++ backend/src/config/session.js
@@ -6,13 +6,19 @@
  */
 
 // Imports
-const session = require("express-session")
+const session = require("express-session");
+const mongoose = require("mongoose");
+const { MongoStore } = require("connect-mongo");
 
 // Constants
 const { SESSION_NAME } = require("../constants");
 
 module.exports = session({
     name: SESSION_NAME,
+    store: new MongoStore({
+        mongoUrl: process.env.MONGO_URI,
+        mongooseConnection: mongoose.connection,
+    }),
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
