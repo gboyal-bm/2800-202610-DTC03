7e95c7e330f25ddca27d15702750390ba16231c7
Al0102 <alu59@my.bcit.ca>
2026-05-20 21:05:41 -0700

    Remove weird import

--- shadesmar/src/Pages/profile.tsx
+++ shadesmar/src/Pages/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/authContext";
 import { ShadesmarApi } from "../utils/shadesmar_api";
 import { BadgeCard } from "../components/badgeCard";
